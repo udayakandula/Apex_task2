8de9faa995d73de5a667c3ea2a849d6fb44b69df
udayakandula <udayakandula06@gmail.com>
2025-05-26 00:40:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function validateForm() {
+console.log("validateForm called");
+  let userName = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let email = document.getElementById("email").value;
+ 
+
+  let nameRegex = /^[A-Za-z]{6}$/;
+  let mailRegex = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  
+  // Validate username
+  if (!nameRegex.test(userName)) {
+    alert("Username must contain only alphabets and be at least 6 characters long.");
+    return false;
+  }
+
+  // Validate password
+  if (password.length < 6) {
+    alert("Password must be at least 6 characters long.");
+    return false;
+  }
+
+  // Validate email
+  if (!mailRegex.test(email)) {
+
+    alert("Invalid email format.");
+    return false;
+  }
+
+ window.location.href="todo.html";
+ return false;
+}
+
+window.validateForm = validateForm;
+
+
